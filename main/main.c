0eb2f9bd67b6ede4e29aa6b1abfafddcdbb3e8c2
lfelipeep <luisfecardozoherrada@gmail.com>
2025-09-20 15:50:00 -0500

    Update main.c

--- main/main.c
+++ main/main.c
@@ -2,7 +2,7 @@
 
 int main() {
     int opcion, id, cantidad, i;
-    int carrito[6] = {0}; // Cantidad de cada producto
+    int carrito[6] = {0}; 
     int precios[6] = {12000, 15000, 18000, 20000, 22000, 25000};
     char *productos[6] = {
         "Sandwich Jamon/Queso + Papas",
@@ -28,14 +28,14 @@ int main() {
         scanf("%d", &opcion);
 
         switch(opcion) {
-            case 1: // Ver menú
+            case 1: 
                 printf("\n=== MENU DE SANDWICHES ===\n");
                 for(i = 0; i < 6; i++) {
                     printf("%d. %s - $%d\n", i+1, productos[i], precios[i]);
                 }
                 break;
 
-            case 2: // Agregar al carrito
+            case 2: 
                 printf("\n=== AGREGAR SANDWICH ===\n");
                 printf("Ingrese el ID del sandwich (1-6): ");
                 scanf("%d", &id);
@@ -57,7 +57,7 @@ int main() {
                 printf("¡Agregado al carrito!\n");
                 break;
 
-            case 3: // Ver carrito
+            case 3: 
                 printf("\n=== CARRITO DE COMPRAS ===\n");
                 total = 0;
                 
@@ -78,7 +78,7 @@ int main() {
                 }
                 break;
 
-            case 4: // Confirmar pedido
+            case 4: 
                 total = 0;
                 for(i = 0; i < 6; i++) {
                     total += carrito[i] * precios[i];
@@ -91,14 +91,14 @@ int main() {
                     printf("Total a pagar: $%d\n", total + domicilio);
                     printf("¡Pedido confirmado! Gracias por su compra.\n");
                     
-                    // Vaciar carrito
+                    
                     for(i = 0; i < 6; i++) {
                         carrito[i] = 0;
                     }
                 }
                 break;
 
-            case 5: // Salir
+            case 5: 
                 printf("¡Gracias por usar nuestro servicio de sandwiches!\n");
                 break;
 
@@ -109,4 +109,5 @@ int main() {
     } while(opcion != 5);
 
     return 0;
+
 }
